4c20e5ad0e9198a069acca28fed0b88eb86d983f
g-t-okuniewicz <ted-sites@live.com>
2018-12-17 18:59:09 +0000

    refactored the client for Python 3

--- client/Python/client.py
+++ client/Python/client.py
@@ -1,8 +1,18 @@
 #Base Python client for MEng in IoT Assignment
 #consumes data from IoT Gateway
-import urllib2
+#import urllib2
 
-response = urllib2.urlopen('http://localhost:8080/')
-resp = response.read()
+#response = urllib2.urlopen('http://localhost:8080/')
+#resp = response.read()
 
-print resp
+#print resp
+
+import urllib3
+
+http = urllib3.PoolManager()
+
+url = 'http://localhost:8080/'
+
+response = http.request('GET', url)
+
+print(response.data)
