8f97061b77e35ce154c55c095f78fd833af2d2e1
g-t-okuniewicz <ted-sites@live.com>
2018-12-18 12:50:48 +0000

    cleaned up the node's main a little

--- node/main.cpp
+++ node/main.cpp
@@ -2,11 +2,9 @@
 #include <iostream>
 #include <unistd.h>
 
-//static const char *s_http_port = "8080";
+static const char *url = "localhost:8080/sendreading";
 
 static void ev_handler(struct mg_connection *c, int ev, void *p) {
-
-	std::cout << "Event: " << ev << std::endl;
 
 	/*
 	 * Error handling
@@ -14,6 +12,9 @@ static void ev_handler(struct mg_connection *c, int ev, void *p) {
 	 * MG_EV_POLL, MG_EV_CONNECT, MG_EV_SEND, and MG_EV_CLOSE
 	 * If the connection fails MG_EV_SEND is not generated.
 	 */
+
+	//std::cout << "Event: " << ev << std::endl;
+
 	static bool data_sent = false;
 
 	if(ev == MG_EV_SEND)
@@ -33,16 +34,15 @@ int main() {
 
 	std::cout << "Starting the node" << std::endl;
 
-
-
 	while(1) {
 		mg_mgr_init(&mgr, NULL);
 
 		mg_connect_http(&mgr,
 				ev_handler,
-				"localhost:8080/sendreading",
+				url,
 				"Content-Type: text/plain\r\n",
-				"<reading><time>12:00</time><temperature>25</temperature></reading>");
+				"<reading><time>12:00</time><temperature>25</temperature></reading>"
+				);
 
 		mg_mgr_free(&mgr);
 		usleep(3000000);
